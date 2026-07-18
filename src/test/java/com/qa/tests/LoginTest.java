4b8b4b4d95c685702444f8111d6aca1a38475a88
Satyam <cheerasatyam@gmail.com>
2026-07-18 12:38:34 +0530

    updated LoginTest class

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -12,7 +12,7 @@ import com.qa.baseTest.BaseTest;
 import com.qa.framework.config.ConfigReader;
 
 public class LoginTest extends BaseTest{
-	
+	//updated postgres
 	LoginPage loginpage;
 
 	@BeforeMethod
